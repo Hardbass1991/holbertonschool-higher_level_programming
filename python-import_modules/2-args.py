f61eed8f5813895003e19145ab31dbc3ab2e05be
Hardbass1991 <midailsorridente@gmail.com>
2022-09-06 13:02:19 -0700

    added script that prints out argv

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from sys import argv
+    n = len(argv) - 1
+    if (n == 0):
+        print(f"{n} arguments.")
+    elif (n == 1):
+        print(f"{n} argument:")
+    else:
+        print(f"{n} arguments:")
+    for i in range(1, n + 1):
+        print(f"{i}: {argv[i]}")
