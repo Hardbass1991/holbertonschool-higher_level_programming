9698b4882fda3037004c57277a1d53b860e1f286
Hardbass1991 <midailsorridente@gmail.com>
2022-09-13 09:05:13 -0700

    deleted except

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,11 +2,8 @@
 def safe_print_list_integers(my_list=[], x=0):
     n = 0
     for i in range(x):
-        try:
-            if type(my_list[i]) == int:
-                print("{:d}".format(my_list[i]), end='')
-                n += 1
-        except IndexError:
-            break
+        if type(my_list[i]) == int:
+            print("{:d}".format(my_list[i]), end='')
+            n += 1
     print()
     return n
