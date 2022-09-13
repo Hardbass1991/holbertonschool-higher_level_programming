f111a229c6b0d9d42d4a0781cae5b160e1d84ea0
Hardbass1991 <midailsorridente@gmail.com>
2022-09-13 09:09:22 -0700

    changed except statement for a useless one, so it fails

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,8 +2,9 @@
 def safe_print_list_integers(my_list=[], x=0):
     n = 0
     for i in range(x):
-        if type(my_list[i]) == int:
-            print("{:d}".format(my_list[i]), end='')
-            n += 1
-    print()
-    return n
+        try:
+            if type(my_list[i]) == int:
+                print("{:d}".format(my_list[i]), end='')
+                n += 1
+        except ZeroDivisionError:
+            break
