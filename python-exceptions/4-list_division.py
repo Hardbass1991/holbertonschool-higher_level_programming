ab1dfd4181f9ac772c3f5c931a13214012b920d9
Hardbass1991 <midailsorridente@gmail.com>
2022-09-13 09:47:01 -0700

    added script that divides elements in two lists, handling exceptions

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    res = []
+    for i in range(list_length):
+        n = 0
+        try:
+            n = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+        except TypeError:
+            print("wrong type")
+        except IndexError:
+            print("out of range")
+        res.append(n)
+    return res
