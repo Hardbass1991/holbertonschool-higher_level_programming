292a2c67a8f04e50369c3d8a989c91f11c146256
Hardbass1991 <midailsorridente@gmail.com>
2022-09-19 10:41:43 -0700

    added script that defines a class with a private attribute

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module defining a square
+"""
+
+
+class Square:
+    """A square
+    """
+    def __init__(self, size):
+        """Initialization of square
+
+        Args:
+            size (int): Size of square
+        """
+        self.__size = size
