1dfe0e876331cce2a555a6937e65dad4282437a6
Hardbass1991 <midailsorridente@gmail.com>
2022-09-20 17:46:07 -0700

    added new tests

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -9,6 +9,12 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     def test_max(self):
         self.assertAlmostEqual(max_integer([1, -4, 69]), 69)
+        self.assertAlmostEqual(max_integer([678, -4, 69]), 678)
+        self.assertAlmostEqual(max_integer([1, 182, 69]), 182)
+        self.assertAlmostEqual(max_integer([1, -4, 45]), 45)
+        self.assertAlmostEqual(max_integer([-1, -4, -69]), -1)
+        self.assertAlmostEqual(max_integer([]), None)
+        self.assertAlmostEqual(max_integer([1]), 1)
 
     def test_types(self):
         self.assertRaises(TypeError, max_integer, ["s", 1, 4, 34])
