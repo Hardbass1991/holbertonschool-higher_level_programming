595f276e5e4349f4869f12a5f26e930257c73123
Hardbass1991 <midailsorridente@gmail.com>
2022-09-27 10:31:41 -0700

    corrected bases array

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -6,6 +6,6 @@ belongs to input class, or a class that inherits from it"""
 def is_kind_of_class(obj, a_class):
     """function that returns whether obj is an instance of a_class
     or of another class that inherits from it"""
-    if type(obj) == a_class or a_class in type(obj).__bases__:
+    if type(obj) == a_class or a_class in type(obj).__mro__:
         return True
     return False
