302691c6b02a4ac457cbc569bc9ea02fde821884
Hardbass1991 <midailsorridente@gmail.com>
2022-09-27 09:37:49 -0700

    added documentation

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""Module featuring a class that inherits from list"""
+
+
 class MyList(list):
+    """class that inherits from list"""
     def print_sorted(self):
+        """method that prints sorted list"""
         print(sorted(self))
