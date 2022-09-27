c42ce683ce8622793c0874106baf51d8d8899320
Hardbass1991 <midailsorridente@gmail.com>
2022-09-27 12:20:05 -0700

    corrected spacing

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,6 +2,7 @@
 """modules that defines a class regarding a geometry"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """class representing a rectangle"""
     def __init__(self, width, height):
