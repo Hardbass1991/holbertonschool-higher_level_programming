14d9a610ad885240c7ce0fa12bf0a08835e24b20
Hardbass1991 <midailsorridente@gmail.com>
2022-09-27 11:07:42 -0700

    added method that validates number

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""modules that defines a class regarding a geometry"""
+
+
+class BaseGeometry:
+    """class about geometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
