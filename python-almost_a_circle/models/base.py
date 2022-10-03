12df5200a10a021fa0325c449c63b227de174741
HardBass1991 <midailsorridente@gmail.com>
2022-10-03 10:22:49 -0700

    corrected spacing

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """module defining basic class"""
 
+
 class Base:
     """class with an instance counter"""
     __nb_objects = 0
 
     def __init__(self, id=None):
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             self.__class__.__nb_objects += 1
