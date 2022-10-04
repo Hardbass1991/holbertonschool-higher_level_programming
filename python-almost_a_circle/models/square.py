bc33000e0e714e6bedc912755e9547d9998197ac
HardBass1991 <midailsorridente@gmail.com>
2022-10-04 09:51:57 -0700

    added update method

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -24,3 +24,26 @@ class Square(Rectangle):
             raise ValueError("width must be > 0")
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """method that updates instance attributes"""
+        if not args:
+            # print(super().__dict__)
+            for k, v in kwargs.items():
+                if k == 'id':
+                    self.__dict__[k] = v
+                elif k == 'size':
+                    self.__dict__["_Rectangle__width"] = v
+                    self.__dict__["_Rectangle__height"] = v
+                else:
+                    self.__dict__["_Rectangle__" + k] = v
+        else:
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[i]
+                elif i == 1:
+                    self.size = args[i]
+                elif i == 2:
+                    self.x = args[i]
+                elif i == 3:
+                    self.y = args[i]
