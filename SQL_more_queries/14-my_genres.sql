ed0153f9e4e8322da1bf9f035c30443e4281a104
Hardbass1991 <midailsorridente@gmail.com>
2022-11-08 07:17:27 -0800

    added script that does a double JOIN and filters data

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- script that lists all genres of a tv show
+SELECT tv_genres.name
+FROM tv_shows 
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE title = 'Dexter'
