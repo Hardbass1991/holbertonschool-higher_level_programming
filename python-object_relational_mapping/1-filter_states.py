436f4d78f94389d004a3a45ef7340d336cd0a04c
Hardbass1991 <midailsorridente@gmail.com>
2022-11-08 10:58:09 -0800

    added BINARY to LIKE

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     a = list(sys.argv)
     db = MySQLdb.connect(host="localhost", user=a[1], passwd=a[2], db=a[3])
     cur = db.cursor()
-    q = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id"
+    q = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id"
     number_of_rows = cur.execute(q)
     rows = cur.fetchall()
     for row in rows:
