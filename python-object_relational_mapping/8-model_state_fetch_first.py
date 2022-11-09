9ea1b5c8f1a43a9db5f720df4488bec0cfda61be
Hardbass1991 <midailsorridente@gmail.com>
2022-11-08 18:10:47 -0800

    added script that show first element in table, using sqlalchemy

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""script that lists the first State object from the database hbtn_0e_6_usa"""
+if __name__ == '__main__':
+    from sqlalchemy import (create_engine)
+    from model_state import Base, State
+    import sys
+
+    a = sys.argv
+    acc = 'mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine(acc.format(a[1], a[2], a[3]), pool_pre_ping=True)
+
+    from sqlalchemy.orm import sessionmaker
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.query(State).first()
+    print(f"1: {result.name}")
