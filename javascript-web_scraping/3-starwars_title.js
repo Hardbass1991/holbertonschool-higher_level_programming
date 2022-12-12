96af18578debdaafd340cf619b0d64fd25a1d706
Hardbass1991 <midailsorridente@gmail.com>
2022-12-12 17:09:57 +0000

    added script that fetches json from api

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const request = require('request');
+let url = 'https://swapi-api.hbtn.io/api/films/';
+url += process.argv[2];
+
+request(url, function (error, response) {
+  const body = JSON.parse(response.body);
+  console.log(body.title);
+});
